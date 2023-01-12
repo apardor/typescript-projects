f62ec4ba6e77196492bc5de25305a31d718da659
Andrés Pardo Rodríguez <andres@Andress-MacBook-Pro.local>
2023-01-12 17:25:07 +0100

    jsonwt working

--- CRUD/server/src/modules/auth.ts
+++ CRUD/server/src/modules/auth.ts
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken'
 import  express, { Application, Request, Response, NextFunction} from 'express'
+import { userInfo } from 'os'
 
 
 export const createJWT = (user) => {
@@ -20,4 +21,24 @@ export const protect = (req: Request, res: Response, next: NextFunction ) => {
         return
     }
 
+    const [, token] = bearer.split(' ')
+
+    if(!token){
+        res.status(401)
+        res.json({message: 'not valid token'})
+        return
+    }
+
+    try{
+        const user = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = user
+        next()
+    } catch (e) {
+        console.log(e);
+        res.status(401)
+        res.json({message: 'not valid/true token '})
+        return
+    }
+
+ 
 }
